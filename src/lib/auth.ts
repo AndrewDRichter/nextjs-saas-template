0dfd4b01709411d840539f0994da73b740d2a392
AndrewDRichter <andrewrichter1@hotmail.com>
2025-11-01 10:37:15 -0300

    ok

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import prisma from "./prisma"
+import { Adapter } from "next-auth/adapters"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  trustHost: true,
+  providers: [],
+})
